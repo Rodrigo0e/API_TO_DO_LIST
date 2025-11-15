522dda735db8a872594b3b8796d1f115821b9ec1
Rodrigo0e <rodrigor1r2r3@gmail.com>
2025-11-15 13:27:35 -0300

    feat: Retirando database que estava sendo criado no fora da pasta

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ import { Sequelize } from "sequelize";
 // Cria conexão com o banco SQLite e define onde o arquivo será salvo
 const db = new Sequelize({
   dialect: process.env.DB_DIALECT || "sqlite",
-  storage: process.env.DB_STORAGE || "./database.sqlite"
+  storage: process.env.DB_STORAGE || "./src/database/database.sqlite"
 });
 
 // Exporta a conexão para ser usada nos models
